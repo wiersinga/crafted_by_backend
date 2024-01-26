9024a7433cc8f6cbb6876c1976c9b89e986803c8
wiersinga <wiersingaolfa@yahoo.fr>
2024-01-26 16:38:31 +0100

    Create ProductPolicy

--- backend/app/Http/Controllers/Api/ProductController.php
+++ backend/app/Http/Controllers/Api/ProductController.php
@@ -10,6 +10,7 @@ use App\Models\Material;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 
@@ -37,6 +38,7 @@ class ProductController extends Controller
 
     public function update(UpdateProductRequest $request, product $product)
     {
+        dd(Auth::user()->can('update'));
         $product->update($request->all());
         return new ProductResource($product);
     }
---
+++ backend/app/Policies/ProductPolicy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Product;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ProductPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Product $product): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     * @param User $user
+     * @param $product
+     * @return bool
+     */
+    public function create(User $user, $product): bool
+    {
+        return ($user->id === $product->artist_id);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Product $product, Role $role): bool
+    {
+
+        //dd($user->role->name);
+
+        return ($role->name === 'admin' || $user->id === $product->artist_id);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Product $product): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Product $product): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Product $product): bool
+    {
+        //
+    }
+}
